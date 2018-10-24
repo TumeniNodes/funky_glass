f5981ee0bc9609c4e1e3710fec2a7f4fda973144
TumeniNodes <tumeninodes@gmail.com>
2018-10-23 23:20:22 -0400

    Add files via upload

--- funky_glass.lua
+++ funky_glass.lua
@@ -22,3 +22,27 @@ minetest.register_node("funky_glass:asgcl", {
 			},
 		},
 	}})
+
+
+--animated, soft glowing, colored obsidian lights [asgcol] even lamer
+
+minetest.register_node("funky_glass:asgcol", {
+	description = "Funky Glass",
+	drawtype = "glasslike",
+	paramtype = "light",
+	use_texture_alpha = true,
+	light_source = 6,
+	sunlight_propagates = true,
+	is_ground_content = false,
+	groups = {cracky = 3, oddly_breakable_by_hand = 3},
+	drop = {1},
+	tiles = {{
+			name = "default_obsidian_glass.png^glass_flashy.png", 
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2,
+			},
+		},
+	}})
